2ff39101b65debbdf07f04b8319a370fc1fae9de
LoveCany <admin@lovecany.me>
2023-04-23 03:10:07 +0900

    change default tsdm domain

--- src/plugins/tsdm/config.py
+++ src/plugins/tsdm/config.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel
 class Config(BaseModel):
     # Your Config Here
     fastapi_reload: bool = True
-    tsdm_base_url: str = "https://www.tsdm39.net"
+    tsdm_base_url: str = "https://www.tsdm39.com"
     tsdm_data_dir: str = 'data'
     tsdm_username: str = ""
     tsdm_password: str = ""
